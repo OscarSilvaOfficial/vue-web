86eccf53ff1254a41f70d2c3abc55f9662b3a056
OscarSilvaOfficial <oscarkaka222@gmail.com>
2020-12-07 19:42:49 -0300

    add

---
+++ src/components/Interface.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar
+      app
+      color="#6be68e"
+      flat
+    >
+      <v-avatar
+        color="primary"
+        size="40"
+      ><img src="/quiteja-icon.png" alt="QuiteJá"></v-avatar>
+
+      <v-tabs
+        centered
+        class="ml-n9"
+        color="dark darken-1"
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link"
+        >
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+    </v-app-bar>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="8"
+          >
+
+            <Table />
+
+          </v-col>
+
+          <v-col
+            cols="12"
+            sm="2"
+          >
+            <v-sheet
+              rounded="lg"
+              min-height="268"
+            >
+              <!--  -->
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Table from './Table';
+
+export default {
+    components: {
+      Table
+    },
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Logs',
+      ],
+    }),
+  }
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="data-table">
+    <v-data-table
+      v-model="selected"
+      :headers="headers"
+      :items="desserts"
+      :single-select="singleSelect"
+      item-key="name"
+      show-select
+      class="elevation-1"
+    >
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import api from '../services/api';
+
+  export default {
+    data () {
+      return {
+        singleSelect: false,
+        selected: [],
+        headers: [
+          {
+            text: 'Dessert (100g serving)',
+            align: 'start',
+            sortable: false,
+            value: 'name',
+          },
+          { text: 'Calories', value: 'calories' },
+          { text: 'Fat (g)', value: 'fat' },
+          { text: 'Carbs (g)', value: 'carbs' },
+          { text: 'Protein (g)', value: 'protein' },
+          { text: 'Iron (%)', value: 'iron' },
+        ],
+        desserts: [],
+      }
+    },
+    mounted() {
+      api.get('/api')
+        .then((res) => {
+          console.log(res.data)
+          this.desserts = res.data
+        })
+    },
+  }
+</script>
+<style>
+
+</style>
