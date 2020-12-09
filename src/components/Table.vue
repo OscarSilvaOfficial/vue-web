65e9b650aea805dff247f395f76c09c5179a1e7c
OscarSilvaOfficial <oscarkaka222@gmail.com>
2020-12-09 15:25:01 -0300

    Adicionando search nas tabelas e alterando algumas props

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,15 +1,38 @@
 <template>
   <div class="data-table">
-    <v-data-table
-      v-model="selected"
-      :headers="headers"
-      :items="desserts"
-      :single-select="singleSelect"
-      item-key="name"
-      show-select
-      class="elevation-1"
-    >
-    </v-data-table>
+    <v-card>
+      <v-card-title>
+        Scheduler
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="mdi-magnify"
+          label="Localizar"
+          single-line
+          hide-details
+          class="search-bar"
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table
+        :headers="cabecalhos"
+        :items="dados"
+        :single-select="singleSelect"
+        item-key="name"
+        show-select
+        class="elevation-1"
+        :search="search"
+        :footer-props="{
+          showFirstLastPage: true,
+          firstIcon: 'mdi-arrow-collapse-left',
+          lastIcon: 'mdi-arrow-collapse-right',
+          prevIcon: 'mdi-minus',
+          nextIcon: 'mdi-plus',
+          'items-per-page-text':'Itens por Página',
+        }"
+      >
+      </v-data-table>
+      
+    </v-card>
   </div>
 </template>
 
@@ -18,42 +41,49 @@ import api from '../services/api';
 import axios from 'axios';
 import { stringToDate } from '../services/conversor'
 
-  export default {
-    data () {
-      return {
-        singleSelect: false,
-        selected: [],
-        headers: [
-          {
-            text: 'Tarefa',
-            align: 'start',
-            sortable: false,
-            value: 'name',
-          },
-          { text: 'Cron', value: 'trigger' },
-          { text: 'Ultima Execução', value: 'last_run_time' },
-          { text: 'Próxima Execução', value: 'next_run_time' },
-        ],
-        desserts: [],
-      }
-    },
-    mounted() {
-      api.get('/scheduler/jobs')
-        .then((res) => {
-          // Conversão da data enviada pela API
-          for(const data of res.data) {
-            data.last_run_time = stringToDate(data.last_run_time)
-            data.next_run_time = stringToDate(data.next_run_time)
+export default {
+  data() {
+    return {
+      singleSelect: false,
+      search: '',
+      cabecalhos: [
+        {
+          text: 'Tarefa',
+          align: 'start',
+          sortable: false,
+          value: 'name',
+        },
+        { text: 'Cron', value: 'trigger' },
+        { text: 'Ultima Execução', value: 'last_run_time' },
+        { text: 'Próxima Execução', value: 'next_run_time' },
+      ],
+      dados: [],
+    }
+  },
+  mounted() {
+    api.get('/scheduler/jobs')
+      .then((res) => {
+        // Conversão da data enviada pela API
+        for(const data of res.data) {
+          data.next_run_time = stringToDate(data.next_run_time)
+          data.last_run_time = stringToDate(data.last_run_time)
+          if(!data.last_run_time) {
+            data.last_run_time = 'Não executado'.toUpperCase()
           }
-          this.desserts = res.data
-        })
-    },
-  }
+        }
+        this.dados = res.data
+      })
+  },
+}
 </script>
 <style>
 
 .data-table {
-  widows: 10rem;
+  width: 100%;
+}
+
+.search-bar {
+  width: 50%;
 }
 
 </style>
